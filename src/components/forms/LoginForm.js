f31fd1aca7f9604e0d441e8e2555ceb5bbec1ee4
StevePurzhash <devolper7@gmail.com>
2018-05-20 23:32:06 +0300

    Make async request to server

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Form, Button} from "semantic-ui-react";
+import {Form, Button, Message} from "semantic-ui-react";
 import Validator from 'validator';
 import InlineError from '../messages/InlineError';
 
@@ -19,11 +19,13 @@ class LoginForm extends React.Component {
         data: {...this.state.data, [e.target.name]: e.target.value}
     });
 
-    onSubmit =() =>{
+    onSubmit = () =>{
       const errors = this.validate(this.state.data);
       this.setState({ errors });
       if(Object.keys(errors).length === 0){
-          this.props.submit(this.state.data);
+          this.setState({ login: true });
+          this.props.submit(this.state.data)
+              .catch(err => this.setState({ errors: err.response.data.errors, loading: false }));
       }
     };
 
@@ -36,9 +38,16 @@ class LoginForm extends React.Component {
     };
 
     render() {
-        const{ data, errors } = this.state;
+        const{ data, errors, loading } = this.state;
+
         return (
-            <Form onSubmit={this.onSubmit}>
+            <Form onSubmit={this.onSubmit} loading={loading}>
+                { errors.global && (
+                    <Message negative >
+                    <Message.Header>Something went wrong</Message.Header>
+                    <p>{errors.global}</p>
+                </Message>
+                )}
                 <Form.Field error={!!errors.email}>
                     <label htmlFor="email">Email</label>
                     <input type="email"
--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,11 +1,14 @@
 import React  from "react";
+import PropTypes from 'prop-types';
 import LoginForm from "../forms/LoginForm";
+import { connect } from 'react-redux';
+import { login } from '../../actions/auth';
+
 
 class LoginPage extends React.Component{
 
-    submit = (data) =>{
-        console.log(data);
-    };
+    submit = data =>
+        this.props.login(data).then(() => this.props.history.push("/"));
 
     render(){
         return(
@@ -18,6 +21,11 @@ class LoginPage extends React.Component{
     }
 }
 
+LoginPage.propTypes = {
+    history: PropTypes.shape({
+        push: PropTypes.func.isRequired
+    }).isRequired,
+    login: PropTypes.func.isRequired
+};
 
-
-export default LoginPage;
+export default connect(null, { login })(PropTypes)
